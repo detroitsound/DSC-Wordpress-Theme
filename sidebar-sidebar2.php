1df22034832f3fe0e6cbcc6baa6dcd554ac8bcd7
Dylan Box <dylan@wedgedetroit.com>
2014-01-29 00:26:37 -0500

    New Hompage and Styling

--- sidebar-sidebar2.php
+++ sidebar-sidebar2.php
@@ -1,15 +1,11 @@
-				<aside id="sidebar2" class="sidebar" role="complementary">
+<?php if ( is_active_sidebar( 'sidebar2' ) ) : ?>
 
-						<?php if ( is_active_sidebar( 'sidebar2' ) ) : ?>
+	<?php dynamic_sidebar( 'sidebar2' ); ?>
 
-							<?php dynamic_sidebar( 'sidebar2' ); ?>
+<?php else : ?>
 
-						<?php else : ?>
+	<!-- This content shows up if there are no widgets defined in the backend. -->
 
-							<!-- This content shows up if there are no widgets defined in the backend. -->
+	<p>Please activate some Widgets.</p>
 
-							<p>Please activate some Widgets.</p>
-
-						<?php endif; ?>
-
-				</aside>
+<?php endif; ?>
