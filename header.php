6b5ee345c1eba560470048712830a7bdeb4ba1de
Dylan Box <dylan@wedgedetroit.com>
2014-04-11 11:49:12 -0400

    Adding OG tags to header

--- header.php
+++ header.php
@@ -90,6 +90,18 @@
 
 		?>
 
+        <meta property="og:title" content="<?php the_title(); ?>"/>
+        <meta property="og:description" content="<?php echo strip_tags(get_the_excerpt($post->ID)); ?>" />
+        <meta property="og:url" content="<?php the_permalink(); ?>"/>
+        <?php $fb_image = wp_get_attachment_image_src(get_post_thumbnail_id( get_the_ID() ), 'thumbnail'); ?>
+        <?php if ($fb_image) : ?>
+            <meta property="og:image" content="<?php echo $fb_image[0]; ?>" />
+        <?php endif; ?>
+        <meta property="og:type" content="<?php
+            if (is_single() || is_page()) { echo "article"; } else { echo "website";} ?>"
+        />
+        <meta property="og:site_name" content="<?php bloginfo('name'); ?>"/>
+
 	</head>
 
 	<body <?php body_class(); ?>>
