4e103aa6aad136fa1dfa031fc32691dc90875be7
Dylan Box <dylan@wedgedetroit.com>
2015-01-28 23:39:29 -0500

    Allow Editing of Thank You Page

--- thank-you-page.php
+++ thank-you-page.php
@@ -6,7 +6,7 @@ Template Name: Thank You Page
 
 <?php get_header(); ?>
 
-			<div id="content" class="clearfix">
+            <div id="content" class="clearfix">
                 <div class="record-player">
                     <div class="top">
                         <div class="speed-adjust">
@@ -32,11 +32,14 @@ Template Name: Thank You Page
                         </div>
                     </div>
                 </div>
-                <section class="full-list">
-                    <h4 class="subheader">This is possible because of the support from contributors like:</h6>
-                    <p>A Warshaw, Chris Ruffner, Matt Hampel, Brian Boyer, John Dawson, Denise Dalphond, Eric Burgos, Sam Valenti IV, Brian Gillespie, Larry Mandel, Andrew Scott, Joe Germuska, Christopher Handyside, Benjamin, Kevin Dingle, Corrie Carozza, Marke Bieschke, Jesse Wolfe, Bill Knudstrup, Nate Suri, Julie Jurrjens, Jane Lerner, Jason Vazzano, Jason Carter, Kristie Erickson, Paul Schauert, Isaac Moore, Brent and Christi Saindon, Gretchen Lindensmith, Darren Clarke, Angelina Czarnecki, Elizabeth Riley, Melissa Minnich, Liz Warner, Suzanne Heinrichs, lstnr, Ben Alschuler, Meryl Krieger, Aaron Luis Levinson, Heather C, David Schon, Jamie Latendresse, Chela, Richard Remer, Beth Duckles, Carole Keller, Craig Caswell, Nestacat, Addis R. Perez, Christine Luttermoser, Katherine Smith, Lori Deradoorian, Michael Arnold III, Lori Dallava, Angie Linder, Brian Mulloy, Kevin Kapalla, Gabriel Doman, Eric Szulc, Tina Moore, Frederick Frank, Abraham Kadushin, Katie Kazmierczak, Cheryl Walker, Jennifer Washington, John Kosinski II, Mike Rubin, Craig Maki, Michele Kennerly, Piotr Orlov, Mary Rousseaux, H. Kelley, Julie Piacentine, Kimberly Silarski, Elif Celebi Wisecup, Melissa Srbinovich, Jan Dijkers, Kenny Hemler, Jacob Arnold, telephase, Lethal Lisa, Lori Polemenakos, Sady Sullivan, Kevin Zollman, Jeff Sumner, Scott Sparling, Greg Fischer, Jennifer, Denise McGeen, Christine Cover Ferro, Benjamin Schultheis, Theresa O'Dea, JoAnn Rusnak, Lisa Marie, Julia, Drew Matzke, Joel Hetzner, Kevin Pachla, Jeff Howlett, Sam Jones, Andrea Eberle, Brian Anderson, Alec Peterhans, Emily Byk, Paul Thebert, Peggy Bach, Ed Cox, c underwood, Ilja Heitlager, Kimberley Babb, Randy Willis, Rebecca Binno Savage, Anna Gershuny, Maria Annichia Riolo, Jim Anderson, Bill Kinney, Jocelyne Ninneman, Christina Ford, Julie Thomson, Kate Glicksberg, Pamela York, Rich Vander Klok, Wojciech Kawalek, lizh313, Leah Garfield-Wright, Brian, Dana Bartle, Simon Newton, Jess Zimbabwe, Rahul Sebastian, Mariah Cherem, Fabio J. Fernandez, Davidde A. Stella, Jill, Miguel Colmenares, Adam Cherne, GoldenGoddess Kin, Pat Mesiti-Miller, Kendel Ratley, Anthony Shakir, Dave Majkowski, Ed Roth, Steven Mollison, Timothy Howse, Billy Harrington, Brian Church, Larissa Heap, Nayiri Mullinix, Rachel Miller, Sareen Papakhian, Tramaya P. Brown, Shawn Neal, Temnites, Angstrom Hertz, Leo Gillis II, Dave Majkowski</p>
-                </div>
+                <section class="post-content">
+                    <?php while ( have_posts() ) : the_post(); ?>
 
-			</div> <!-- end #content -->
+                        <?php the_content(); ?>
+
+                    <?php endwhile; ?>
+                </section>
+
+            </div> <!-- end #content -->
             <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_directory' ); ?>/stylesheets/thank-you.css" />
 <?php get_footer(); ?>
