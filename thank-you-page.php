6b7e561fab34d4ad665379f17e1e84abef603d41
Dylan Box <dylan@wedgedetroit.com>
2017-02-24 22:09:33 -0500

    Change order of thank you page

--- thank-you-page.php
+++ thank-you-page.php
@@ -7,31 +7,6 @@ Template Name: Thank You Page
 <?php get_header(); ?>
 
             <div id="content" class="clearfix">
-                <div class="record-player">
-                    <div class="top">
-                        <div class="speed-adjust">
-                            <div class="circle"></div>
-                        </div>
-                        <div class="record">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/record.png" alt="Thank You" />
-                        </div>
-                        <div class="tone-arm">
-                            <div class="circle"></div>
-                            <div class="arm"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tone-arm.png" alt="Tone-Arm" /></div>
-                        </div>
-                    </div>
-                    <div class="bottom">
-                        <div class="left">
-                            <div class="record-button"></div>
-                        </div>
-                        <div class="right">
-                            <div class="record-button"></div>
-                            <div class="record-button"></div>
-                            <div class="record-button"></div>
-                            <div class="record-button"></div>
-                        </div>
-                    </div>
-                </div>
                 <section class="post-content">
                     <?php while ( have_posts() ) : the_post(); ?>
 
@@ -39,6 +14,34 @@ Template Name: Thank You Page
 
                     <?php endwhile; ?>
                 </section>
+
+                <div class="clearfix">
+                  <div class="record-player">
+                      <div class="top">
+                          <div class="speed-adjust">
+                              <div class="circle"></div>
+                          </div>
+                          <div class="record">
+                              <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/record.png" alt="Thank You" />
+                          </div>
+                          <div class="tone-arm">
+                              <div class="circle"></div>
+                              <div class="arm"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tone-arm.png" alt="Tone-Arm" /></div>
+                          </div>
+                      </div>
+                      <div class="bottom">
+                          <div class="left">
+                              <div class="record-button"></div>
+                          </div>
+                          <div class="right">
+                              <div class="record-button"></div>
+                              <div class="record-button"></div>
+                              <div class="record-button"></div>
+                              <div class="record-button"></div>
+                          </div>
+                      </div>
+                  </div>
+                </div>
 
             </div> <!-- end #content -->
             <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_directory' ); ?>/stylesheets/thank-you.css" />
